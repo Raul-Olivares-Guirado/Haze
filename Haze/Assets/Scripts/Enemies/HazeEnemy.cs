37b2118cc00fe84f58c5fb02f9fb5e61bd3cfa13
Raul Olivares <kore379@gmail.com>
2023-05-17 18:40:50 +0200

    VFX

--- Haze/Assets/Scripts/Enemies/HazeEnemy.cs
+++ Haze/Assets/Scripts/Enemies/HazeEnemy.cs
@@ -1,19 +1,26 @@
-using Platformer.Mechanics;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
+
+
 
 public class HazeEnemy : MonoBehaviour
 {
 
     public TimerController timerController;
-    
+    [SerializeField]
+    private Volume _volume;
+    private Vignette _vignette;
+    private LensDistortion _lensDistortion;
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
         {
             timerController.EnabledTimer();
-            
+            PostProActive();
         }
     }
 
@@ -22,8 +29,29 @@ public class HazeEnemy : MonoBehaviour
         if (collision.gameObject.CompareTag("Player"))
         {
             timerController.DisabledTimer();
-
+            PostProInActive();
         }
     }
 
+    public void PostProActive()
+    {
+        VolumeProfile profile = _volume.sharedProfile;
+
+        _volume.profile.TryGet(out _vignette);
+        _volume.profile.TryGet(out _lensDistortion);
+        _vignette.intensity.value = 1f;
+        _vignette.center.value = new Vector2(0.5f, 0.61f);
+        _lensDistortion.intensity.value = 0.7f;
+    }
+
+    public void PostProInActive()
+    {
+        VolumeProfile profile = _volume.sharedProfile;
+
+        _volume.profile.TryGet(out _vignette);
+        _volume.profile.TryGet(out _lensDistortion);
+        _vignette.intensity.value = 0.173f;
+        _vignette.center.value = new Vector2(0.5f, 0.5f);
+        _lensDistortion.intensity.value = 0f;
+    }
 }
